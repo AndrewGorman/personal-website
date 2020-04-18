e2e4525f15276d133353e4a6643c0d28dcfea355
Andrew Gorman <andrew@andrewgorman.dev>
2020-04-18 17:05:20 -0500

    Starts homepage, finished blue box

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {BootstrapVue} from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <h1 class="header-name-callout">Andrew Gorman</h1>
+        <div
+            class="blue-content-box"
+        >
+            <h2>Hi, I'm Andrew.</h2>
+            <p class="med-text">
+                Full Stack Web Developer <br />
+                App Developer <br />
+                Photographer <br />
+                Freelancer
+            </p>
+            <b-button>Explore my portfolio</b-button>
+            <div class="contact-icons">
+                <img class="contact-icon" width="100px" src="@/assets/github.png">
+                <img class="contact-icon" width="100px" src="@/assets/email.png">
+                <img class="contact-icon" width="100px" src="@/assets/linkedin.png">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        components: {}
+    }
 </script>
+<style lang="sass">
+    @import '@/styles/variables.sass'
+
+    .header-name-callout
+        position: absolute
+        top: 0.1rem
+        font-size: 2rem
+        left: 50%
+        transform: translateX(-50%)
+        color: $white
+
+    .blue-content-box
+        display: flex
+        background-color: $primary
+        border-radius: 10px
+        margin: -1rem 0 0 -1rem
+        padding: 2rem 1rem 1rem 2rem
+        text-align: center
+        flex-direction: column
+        justify-content: center
+        align-items: center
+        width: calc(50vw - 32px)
+        min-height: 70vh
+
+        h2
+            font-size: 2.5rem
+            margin-bottom: 1rem
+
+        .med-text
+            margin-top: 0
+
+        .contact-icons
+            margin: 4rem 0 2rem 0
+            display: flex
+            flex-direction: row
+            justify-content: space-between
+            align-items: baseline
+            min-width: 350px
+
+            .contact-icon
+                max-width: 69px
+                max-height: 69px
+</style>
